fc7a13790a4489654bb896ce850162f67e36dadc
Shrinivas2708 <ssherikar2005@gmail.com>
2026-01-16 01:54:25 +0530

    feat: integrate Redis for pub/sub functionality in WebSocket server

--- backend/src/ws.ts
+++ backend/src/ws.ts
@@ -1,9 +1,18 @@
-import { WebSocketServer ,WebSocket} from "ws";
-import http from "http"
-import  jwt  from "jsonwebtoken";
+import { WebSocketServer, WebSocket } from "ws";
+import http from "http";
+import jwt from "jsonwebtoken";
 import pkg from "jsonwebtoken";
 import { JWT_SECRET } from "./index.js";
+import { pub, sub } from "./redis.js";
 const { JsonWebTokenError } = pkg;
+interface AuthedSocket extends WebSocket {
+  id?: string;
+  user?: {
+    userId: string;
+    email: string;
+    name: string;
+  };
+}
 type Message =
   | {
       type: "join";
@@ -16,117 +25,143 @@ type Message =
   | {
       type: "exit";
     };
-export default function ws(server : http.Server){
-    
-const ws = new WebSocketServer({ server }, () => {
-  console.log("Socket server running ");
-});
-const heartBeat = setInterval(() => {
-  ws.clients.forEach((socket) => {
-    const s = socket as any;
-    if (s.isAlive === false) return socket.terminate();
-    s.isAlive = false;
-    socket.ping();
-  });
-}, 30_000);
-
 const roomToSockets = new Map<string, Set<WebSocket>>();
 const socketsToRoom = new Map<WebSocket, string>();
-const cleanup = (socket: WebSocket) => {
-  const roomId = socketsToRoom.get(socket);
-  socketsToRoom.delete(socket);
-  if (!roomId) return;
-  const sockets = roomToSockets.get(roomId);
-  sockets?.delete(socket);
-  if (sockets?.size === 0) roomToSockets.delete(roomId);
-};
-interface AuthedSocket extends WebSocket {
-  user?: {
-    userId: string;
-    email: string;
-    name:string
-  };
+const subscribedRooms = new Set<string>();
+
+async function subscribeRoom(roomId: string) {
+  const channel = `room:${roomId}`;
+
+  if (subscribedRooms.has(channel)) return; // 🔒 critical
+
+  await sub.subscribe(channel, (message) => {
+    const event = JSON.parse(message);
+
+    const sockets = roomToSockets.get(event.roomId);
+    if (!sockets) return;
+
+    sockets.forEach((s: AuthedSocket) => {
+      if ((s as any).id === event.senderSocketId) return;
+      s.send(JSON.stringify(event));
+    });
+  });
+
+  subscribedRooms.add(channel);
 }
 
-ws.on("connection", (socket: AuthedSocket, req) => {
-  try {
-    const token = new URL(
-      req.url!,
-      `http://${req.headers.host}`
-    ).searchParams.get("token");
-    if (!token) {
-      socket.close(4001, "Not authorized!");
-      return;
-    }
-    const decoded = jwt.verify(token, JWT_SECRET!) as {
-      userId: string;
-      email: string;
-      name:string
-    };
-    socket.user = {
-      userId: decoded.userId,
-      email: decoded.email,
-      name:decoded.name
-    };
-  } catch (err) {
-    if (err instanceof JsonWebTokenError)
-      return socket.close(4002, "Invalid token");
-    socket.close();
-    return;
-  }
-  (socket as any).isAlive = true;
-
-  socket.on("pong", () => {
-    (socket as any).isAlive = true;
+export default function ws(server: http.Server) {
+  const ws = new WebSocketServer({ server }, () => {
+    console.log("Socket server running ");
   });
+  const heartBeat = setInterval(() => {
+    ws.clients.forEach((socket) => {
+      const s = socket as any;
+      if (s.isAlive === false) return socket.terminate();
+      s.isAlive = false;
+      socket.ping();
+    });
+  }, 30_000);
 
-  socket.on("message", (event) => {
-    let parsedMessage: Message;
+  const cleanup = (socket: WebSocket) => {
+    const roomId = socketsToRoom.get(socket);
+    socketsToRoom.delete(socket);
+    if (!roomId) return;
+    const sockets = roomToSockets.get(roomId);
+    sockets?.delete(socket);
+    if (sockets?.size === 0) roomToSockets.delete(roomId);
+  };
+
+  ws.on("connection", (socket: AuthedSocket, req) => {
+    console.log("socket connected", socket.user?.userId);
+
     try {
-      parsedMessage = JSON.parse(event.toString());
-    } catch (error) {
+      const token = new URL(
+        req.url!,
+        `http://${req.headers.host}`
+      ).searchParams.get("token");
+      if (!token) {
+        socket.close(4001, "Not authorized!");
+        return;
+      }
+      const decoded = jwt.verify(token, JWT_SECRET!) as {
+        userId: string;
+        email: string;
+        name: string;
+      };
+      socket.user = {
+        userId: decoded.userId,
+        email: decoded.email,
+        name: decoded.name,
+      };
+    } catch (err) {
+      if (err instanceof JsonWebTokenError)
+        return socket.close(4002, "Invalid token");
+      socket.close();
       return;
     }
-    if (parsedMessage.type === "join") {
-      const prevRoom = socketsToRoom.get(socket);
-      if (prevRoom) roomToSockets.get(prevRoom)?.delete(socket);
-
-      if (!roomToSockets.has(parsedMessage.roomId)) {
-        roomToSockets.set(parsedMessage.roomId, new Set());
-      }
-      roomToSockets.get(parsedMessage.roomId)?.add(socket);
-      socketsToRoom.set(socket, parsedMessage.roomId);
-    }
-
-    if (parsedMessage.type === "chat") {
-      const roomId = socketsToRoom.get(socket);
-      if (!roomId) return;
-      if (!parsedMessage.message || typeof parsedMessage.message !== "string")
+    (socket as any).isAlive = true;
+    socket.on("pong", () => {
+      (socket as any).isAlive = true;
+    });
+    socket.id = crypto.randomUUID();
+    socket.on("message", (event) => {
+      let parsedMessage: Message;
+      try {
+        parsedMessage = JSON.parse(event.toString());
+      } catch (error) {
         return;
+      }
+      if (parsedMessage.type === "join") {
+        const prevRoom = socketsToRoom.get(socket);
+        if (prevRoom) roomToSockets.get(prevRoom)?.delete(socket);
 
-      roomToSockets.get(roomId)?.forEach((v) => {
-        console.log(parsedMessage.message);
-        if (v != socket)
-          v.send(
-            JSON.stringify({
-              message: parsedMessage.message,
-              email: socket.user?.email,
-              name:socket.user?.name,
-              timestamp:new Date()
-            })
-          );
-      });
-    }
-    if (parsedMessage.type === "exit") {
+        if (!roomToSockets.has(parsedMessage.roomId)) {
+          roomToSockets.set(parsedMessage.roomId, new Set());
+        }
+        roomToSockets.get(parsedMessage.roomId)?.add(socket);
+        socketsToRoom.set(socket, parsedMessage.roomId);
+        subscribeRoom(parsedMessage.roomId);
+      }
+
+      if (parsedMessage.type === "chat") {
+        const roomId = socketsToRoom.get(socket);
+        if (!roomId) return;
+        // if (!parsedMessage.message || typeof parsedMessage.message !== "string")
+        //   return;
+
+        // roomToSockets.get(roomId)?.forEach((v) => {
+        //   console.log(parsedMessage.message);
+        //   if (v != socket)
+        //     v.send(
+        //       JSON.stringify({
+        //         message: parsedMessage.message,
+        //         email: socket.user?.email,
+        //         name: socket.user?.name,
+        //         timestamp: new Date(),
+        //       })
+        //     );
+        // });
+        const event = {
+          roomId,
+          message: parsedMessage.message,
+          sender: socket.user!,
+          senderSocketId: socket.id,
+          timestamp: new Date().toISOString(),
+        };
+        console.log("PUBLISH", roomId, socket.user?.userId);
+
+        pub.publish(`room:${roomId}`, JSON.stringify(event));
+      }
+
+      if (parsedMessage.type === "exit") {
+        cleanup(socket);
+      }
+    });
+    socket.on("close", () => {
       cleanup(socket);
-    }
+    });
   });
-  socket.on("close", () => {
-    cleanup(socket);
+  ws.on("close", () => {
+    clearInterval(heartBeat);
   });
-});
-ws.on("close", () => {
-  clearInterval(heartBeat);
-});
-
 }
